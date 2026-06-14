275c76a0e79e9be11a1dafbae2641cc3414e7d4e
sejaldhanade <sejaldhanade@gmail.com>
2026-06-14 18:37:10 +0530

    Added Student Grade Tracker code

--- Task 1_Student Grade Tracker/StudentGradeTracker.java
+++ Task 1_Student Grade Tracker/StudentGradeTracker.java
@@ -1,1 +1,45 @@
+import java.util.*;
 
+public class StudentGradeTracker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int marks[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            System.out.print("Enter marks of Student " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        int highest = marks[0];
+        int lowest = marks[0];
+
+        for(int i = 0; i < n; i++) {
+
+            sum += marks[i];
+
+            if(marks[i] > highest) {
+                highest = marks[i];
+            }
+
+            if(marks[i] < lowest) {
+                lowest = marks[i];
+            }
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("\n===== RESULT =====");
+        System.out.println("Average Marks = " + average);
+        System.out.println("Highest Marks = " + highest);
+        System.out.println("Lowest Marks = " + lowest);
+
+        sc.close();
+    }
+}
