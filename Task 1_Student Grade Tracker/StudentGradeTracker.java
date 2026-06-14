e952a618aa37e18b09e1d34d5243952001fde58f
sejaldhanade <sejaldhanade@gmail.com>
2026-06-14 18:55:21 +0530

    Updated Student Grade Tracker with names and grades

--- Task 1_Student Grade Tracker/StudentGradeTracker.java
+++ Task 1_Student Grade Tracker/StudentGradeTracker.java
@@ -2,43 +2,73 @@ import java.util.*;
 
 public class StudentGradeTracker {
 
+    public static String calculateGrade(int marks) {
+        if (marks >= 90) {
+            return "A+";
+        } else if (marks >= 80) {
+            return "A";
+        } else if (marks >= 70) {
+            return "B";
+        } else if (marks >= 60) {
+            return "C";
+        } else if (marks >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== STUDENT GRADE TRACKER =====");
+
         System.out.print("Enter number of students: ");
         int n = sc.nextInt();
+        sc.nextLine();
 
-        int marks[] = new int[n];
+        String[] names = new String[n];
+        int[] marks = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            System.out.print("Enter marks of Student " + (i + 1) + ": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name of Student " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+
+            System.out.print("Enter marks of " + names[i] + ": ");
             marks[i] = sc.nextInt();
+            sc.nextLine();
         }
 
         int sum = 0;
         int highest = marks[0];
         int lowest = marks[0];
 
-        for(int i = 0; i < n; i++) {
-
+        for (int i = 0; i < n; i++) {
             sum += marks[i];
 
-            if(marks[i] > highest) {
+            if (marks[i] > highest) {
                 highest = marks[i];
             }
 
-            if(marks[i] < lowest) {
+            if (marks[i] < lowest) {
                 lowest = marks[i];
             }
         }
 
         double average = (double) sum / n;
 
-        System.out.println("\n===== RESULT =====");
-        System.out.println("Average Marks = " + average);
-        System.out.println("Highest Marks = " + highest);
-        System.out.println("Lowest Marks = " + lowest);
+        System.out.println("\n===== STUDENT REPORT =====");
+        System.out.println("Name\t\tMarks\tGrade");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(names[i] + "\t\t" + marks[i] + "\t" + calculateGrade(marks[i]));
+        }
+
+        System.out.println("\n===== SUMMARY =====");
+        System.out.println("Average Marks: " + average);
+        System.out.println("Highest Marks: " + highest);
+        System.out.println("Lowest Marks: " + lowest);
 
         sc.close();
     }
